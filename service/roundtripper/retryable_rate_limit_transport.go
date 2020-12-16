fbbcc22adccb09c27eabc1534ec7b500ba02441a
Dany Svirsky <dany.svirsky@broadcom.com>
2020-12-16 11:32:28 +0200

    PLT-1285 - fixed rate limiter

--- service/roundtripper/retryable_rate_limit_transport.go
+++ service/roundtripper/retryable_rate_limit_transport.go
@@ -1,9 +1,11 @@
 package roundtripper
 
 import (
-	"fmt"
-	"golang.org/x/oauth2"
+	"bytes"
+	"context"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 )
@@ -11,34 +13,45 @@ import (
 type RetryableRateLimitTransport struct {
 	roundTripper http.RoundTripper
 	sleepBetweenRetries time.Duration
+	retrySleepJitter int
 }
 
-func NewRetryableRateLimitTransport(millsBetweenRetries int, roundTripper http.RoundTripper) http.RoundTripper {
+func NewRetryableRateLimitTransport(millsBetweenRetries int, retrySleepJitter int, roundTripper http.RoundTripper) http.RoundTripper {
+	rand.Seed(time.Now().UnixNano())
 
 	return &RetryableRateLimitTransport{
 		roundTripper: roundTripper,
 		sleepBetweenRetries: time.Duration(millsBetweenRetries) * time.Millisecond,
+		retrySleepJitter: retrySleepJitter,
 	}
 }
 
 func (t *RetryableRateLimitTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 
-	response, err := t.roundTripper.RoundTrip(r)
-
-	if err != nil {
-		switch err.(type) {
-		case *oauth2.RetrieveError:
-			// in case of rate-limit error, retry after sleepBetweenRetries duration
-			if err.(*oauth2.RetrieveError).Response.StatusCode == 429 {
-				fmt.Println("Retry request due to rate limit error.")
-				log.Printf("[DEBUG] - Retry request due to rate limit error.")
-				time.Sleep(t.sleepBetweenRetries)
-				return t.roundTripper.RoundTrip(r)
-			}
-		default:
-			return nil, err
-		}
+	// Duplicate the request
+	var body bytes.Buffer
+	if r.ContentLength > 0 {
+		body.ReadFrom(r.Body)
 	}
 
+	r2 := r.Clone(context.Background())
+	r2.Body = ioutil.NopCloser(bytes.NewReader(body.Bytes()))
+
+	r.Body = ioutil.NopCloser(&body)
+
+	response, err := t.roundTripper.RoundTrip(r)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode == 429 {
+		log.Printf("[DEBUG] Retry request due to rate limit error.")
+
+		// Jitter sleep time to spread the retries more evenly
+		sleep := t.sleepBetweenRetries + (time.Duration(rand.Intn(t.retrySleepJitter)) * time.Millisecond) - (time.Duration(t.retrySleepJitter / 2) * time.Millisecond)
+		time.Sleep(sleep)
+
+		return t.RoundTrip(r2)
+	}
 	return response, err
 }
