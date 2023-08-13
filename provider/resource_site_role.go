e33ad242488673cd3293084183e93a67d529d6fd
yuvalstyr <yuval.styr@broadcom.com>
2023-08-13 10:16:09 +0300

    AC-32436 | rbac resources

--- provider/resource_site_role.go
+++ provider/resource_site_role.go
@@ -1,9 +1,11 @@
 package provider
 
 import (
+	"context"
 	"github.com/Broadcom/terraform-provider-luminate/service"
 	"github.com/Broadcom/terraform-provider-luminate/service/dto"
 	"github.com/Broadcom/terraform-provider-luminate/utils"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/pkg/errors"
 	"log"
@@ -19,36 +21,36 @@ func LuminateSiteRole() *schema.Resource {
 		ForceNew:     true,
 	}
 	return &schema.Resource{
-		Schema: siteSchema,
-		Create: resourceCreateSiteRole,
-		Read:   resourceReadSiteRole,
-		Delete: resourceDeleteRoleBindings,
+		Schema:        siteSchema,
+		CreateContext: resourceCreateSiteRole,
+		ReadContext:   resourceReadSiteRole,
+		DeleteContext: resourceDeleteRoleBindings,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
 	}
 }
 
-func resourceCreateSiteRole(d *schema.ResourceData, m interface{}) error {
+func resourceCreateSiteRole(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	log.Println("[Info] Creating Site Role")
 	client, ok := m.(*service.LuminateService)
 	if !ok {
-		return errors.New("invalid client ")
+		return diag.FromErr(errors.New("invalid client "))
 	}
 
 	baseFields, err := extractBaseFields(d)
 	if err != nil {
-		return errors.Wrap(err, "extract site role base fields error")
+		return diag.FromErr(errors.Wrap(err, "extract site role base fields error"))
 	}
 	if !utils.ValidateSiteRole(baseFields.RoleType) {
-		return errors.New("invalid role type")
+		return diag.FromErr(errors.New("invalid role type"))
 	}
 
 	siteID := d.Get("site_id").(string)
 
 	entity, err := getEntityByRoleBindings(client, baseFields)
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	var tenantRole = dto.CreateSiteRoleDTO{
@@ -61,29 +63,29 @@ func resourceCreateSiteRole(d *schema.ResourceData, m interface{}) error {
 
 	roleBindings, err := client.RoleBindingsAPI.CreateSiteRoleBindings(&tenantRole)
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	d.SetId(roleBindings[0].ID)
-	return nil
+	return resourceReadSiteRole(ctx, d, m)
 }
 
-func resourceReadSiteRole(d *schema.ResourceData, m interface{}) error {
+func resourceReadSiteRole(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	log.Println("[Info] Reading Site Role")
 	client, ok := m.(*service.LuminateService)
 	if !ok {
-		return errors.New("invalid client")
+		return diag.FromErr(errors.New("invalid client"))
 	}
 	roleBindingsID := d.Id()
 	roleType := d.Get("role_type").(string)
 	if !utils.ValidateSiteRole(roleType) {
-		return errors.New("invalid role type")
+		return diag.FromErr(errors.New("invalid role type"))
 	}
 	entityID := d.Get("entity_id").(string)
 	siteID := d.Get("site_id").(string)
 	role, err := client.RoleBindingsAPI.ReadRoleBindings(roleBindingsID, roleType, entityID, "", siteID)
 	if err != nil {
-		return errors.Wrap(err, "read site role failure")
+		return diag.FromErr(errors.Wrap(err, "read site role failure"))
 	}
 
 	d.SetId(role.ID)
--- provider/resource_tenant_role.go
+++ provider/resource_tenant_role.go
@@ -1,9 +1,11 @@
 package provider
 
 import (
+	"context"
 	"github.com/Broadcom/terraform-provider-luminate/service"
 	"github.com/Broadcom/terraform-provider-luminate/service/dto"
 	"github.com/Broadcom/terraform-provider-luminate/utils"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/pkg/errors"
 	"log"
@@ -12,35 +14,35 @@ import (
 func LuminateTenantRole() *schema.Resource {
 	tenantRoleSchema := LuminateAssignRoleBaseSchema()
 	return &schema.Resource{
-		Schema: tenantRoleSchema,
-		Create: resourceCreateTenantRole,
-		Read:   resourceReadTenantRole,
-		Delete: resourceDeleteRoleBindings,
+		Schema:        tenantRoleSchema,
+		CreateContext: resourceCreateTenantRole,
+		ReadContext:   resourceReadTenantRole,
+		DeleteContext: resourceDeleteRoleBindings,
 		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
+			StateContext: schema.ImportStatePassthroughContext,
 		},
 	}
 }
 
-func resourceCreateTenantRole(d *schema.ResourceData, m interface{}) error {
+func resourceCreateTenantRole(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	log.Println("[Info] Creating Tenant Role")
 	client, ok := m.(*service.LuminateService)
 	if !ok {
-		return errors.New("invalid client ")
+		return diag.FromErr(errors.New("invalid client "))
 	}
 
 	baseFields, err := extractBaseFields(d)
 	if err != nil {
-		return errors.Wrap(err, "extract tenant role base fields error")
+		return diag.FromErr(errors.Wrap(err, "extract tenant role base fields error"))
 	}
 
 	entity, err := getEntityByRoleBindings(client, baseFields)
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 	roleType := d.Get("role_type").(string)
 	if !utils.ValidateTenantRole(roleType) {
-		return errors.New("invalid role type")
+		return diag.FromErr(errors.New("invalid role type"))
 	}
 	var tenantRole = dto.CreateRoleDTO{
 		Role:     roleType,
@@ -49,28 +51,28 @@ func resourceCreateTenantRole(d *schema.ResourceData, m interface{}) error {
 
 	roleBindings, err := client.RoleBindingsAPI.CreateTenantRoleBindings(&tenantRole)
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	d.SetId(roleBindings[0].ID)
-	return nil
+	return resourceReadTenantRole(ctx, d, m)
 }
 
-func resourceReadTenantRole(d *schema.ResourceData, m interface{}) error {
+func resourceReadTenantRole(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	log.Println("[Info] Reading Tenant Role")
 	client, ok := m.(*service.LuminateService)
 	if !ok {
-		return errors.New("invalid client")
+		return diag.FromErr(errors.New("invalid client"))
 	}
 	roleID := d.Id()
 	roleType := d.Get("role_type").(string)
 	if !utils.ValidateTenantRole(roleType) {
-		return errors.New("invalid role type")
+		return diag.FromErr(errors.New("invalid role type"))
 	}
 	entityID := d.Get("entity_id").(string)
 	role, err := client.RoleBindingsAPI.ReadRoleBindings(roleID, roleType, entityID, "", "")
 	if err != nil {
-		return errors.Wrap(err, "read tenant role failure")
+		return diag.FromErr(errors.Wrap(err, "read tenant role failure"))
 	}
 
 	d.SetId(role.ID)
