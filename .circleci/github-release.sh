6adb31d800a5bebacdcc046aea37ba89d7eb1daa
Yoav Drori <yoav.drori@broadcom.com>
2023-12-04 14:49:01 +0200

    PLT-5060 - error handling github-release.sh

--- .circleci/github-release.sh
+++ .circleci/github-release.sh
@@ -6,34 +6,44 @@ GITHUB_BASE_URL="https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCL
 
 AUTH="${GITHUB_ACCESS_TOKEN}"
 
-
-read -r -d '' PAYLOAD << EOF
+read -r -d '' PAYLOAD <<EOF
 {
     "tag_name": "${VERSION}",
     "name": "${VERSION}"
 }
 EOF
 
-RESP=$(curl -X POST -H "Authorization: token ${AUTH}" ${GITHUB_BASE_URL}/releases -d "${PAYLOAD}")
+echo "Creating release $VERSION on URL: $GITHUB_BASE_URL"
+OUTPUT=$(mktemp)
+RESP=$(curl -f -X POST -H "Authorization: token ${AUTH}" ${GITHUB_BASE_URL}/releases -d "${PAYLOAD}" -w "%{response_code}" -o $OUTPUT)
+if [ $? -ne 0 ]; then
+  echo "ERROR: creating the release failed with HTTP/$RESP"
+  cat "$OUTPUT"
+  exit 1
+fi
 
-UPLOAD_URL=$(echo ${RESP} | jq -r .upload_url | cut -f1 -d{ )
+UPLOAD_URL=$(cat ${OUTPUT} | jq -r .upload_url | cut -f1 -d{)
+if [ -z "$UPLOAD_URL" ] || [ "$UPLOAD_URL" == "null" ]; then
+  echo "ERROR: failed to parse upload URL as json (resolved as \"${UPLOAD_URL}\") from body:"
+  cat ${OUTPUT}
+  exit 1
+fi
+
 echo "Upload URL: $UPLOAD_URL"
 
-for FILE in $(ls release)
-do
-    echo "Uploading $FILE"
-    curl -f -X POST \
-        -H "Content-Type: application/octet-stream" \
-        -H "Authorization: token ${AUTH}" \
-        --data-binary @"release/${FILE}" \
-        "${UPLOAD_URL}?name=${FILE}"
-        RETVAL=$?
-        echo ""
-        if [ $RETVAL -ne 0]
-        then
-          echo "Error! Failed to upload $FILE to $UPLOAD_URL - curl returned error #$RETVAL"
-          exit $RETVAL
-        fi
+for FILE in $(ls release); do
+  echo "Uploading $FILE"
+  curl -f -X POST \
+    -H "Content-Type: application/octet-stream" \
+    -H "Authorization: token ${AUTH}" \
+    --data-binary @"release/${FILE}" \
+    "${UPLOAD_URL}?name=${FILE}"
+  RETVAL=$?
+  echo ""
+  if [ $RETVAL -ne 0]; then
+    echo "Error! Failed to upload $FILE to $UPLOAD_URL - curl returned error #$RETVAL"
+    exit $RETVAL
+  fi
 done
 
 exit 0
