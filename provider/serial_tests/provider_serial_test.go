f6b1a54adef8ccd6848bd2c951b6740c50748e4b
yuvalstyr <yuval.styr@broadcom.com>
2024-02-25 14:24:39 +0200

    AC-34599 | add make command, create a serial package

---
+++ provider/serial_tests/provider_serial_test.go
@@ -0,0 +1,47 @@
+package serial_tests
+
+import (
+	"github.com/Broadcom/terraform-provider-luminate/provider"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+var testAccProviders map[string]*schema.Provider
+var newTestAccProviders map[string]func() (*schema.Provider, error)
+var testAccProvider *schema.Provider
+var testAccDomain string
+
+func init() {
+	testAccProvider = provider.Provider()
+	testAccProviders = map[string]*schema.Provider{
+		"luminate": testAccProvider,
+	}
+	newTestAccProviders = make(map[string]func() (*schema.Provider, error))
+	newTestAccProviders["luminate"] = func() (*schema.Provider, error) {
+		return testAccProvider, nil
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := provider.Provider().InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func testAccPreCheck(t *testing.T) {
+	apiEndpoint := os.Getenv("LUMINATE_API_ENDPOINT")
+	if apiEndpoint == "" {
+		t.Fatal("LUMINATE_API_ENDPOINT must be set for acceptance tests")
+	} else {
+		testAccDomain = strings.Replace(apiEndpoint, "api.", "", 1)
+	}
+	if v := os.Getenv("LUMINATE_API_CLIENT_ID"); v == "" {
+		t.Fatal("LUMINATE_API_CLIENT_ID must be set for acceptance tests")
+	}
+	if v := os.Getenv("LUMINATE_API_CLIENT_SECRET"); v == "" {
+		t.Fatal("LUMINATE_API_CLIENT_SECRET must be set for acceptance tests")
+	}
+}
--- provider/serial_tests/resource_integration_test.go
+++ provider/serial_tests/resource_integration_test.go
@@ -1,4 +1,4 @@
-package provider
+package serial_tests
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
