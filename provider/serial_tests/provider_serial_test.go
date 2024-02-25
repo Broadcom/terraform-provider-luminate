df8340a99caf5b07d405ed6ddc5cf4370d861f38
yuvalstyr <yuval.styr@broadcom.com>
2024-02-25 14:27:13 +0200

    AC-34599 | remove unnecessary vars

--- provider/serial_tests/provider_serial_test.go
+++ provider/serial_tests/provider_serial_test.go
@@ -3,22 +3,16 @@ package serial_tests
 import (
 	"github.com/Broadcom/terraform-provider-luminate/provider"
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-var testAccProviders map[string]*schema.Provider
 var newTestAccProviders map[string]func() (*schema.Provider, error)
 var testAccProvider *schema.Provider
-var testAccDomain string
 
 func init() {
 	testAccProvider = provider.Provider()
-	testAccProviders = map[string]*schema.Provider{
-		"luminate": testAccProvider,
-	}
 	newTestAccProviders = make(map[string]func() (*schema.Provider, error))
 	newTestAccProviders["luminate"] = func() (*schema.Provider, error) {
 		return testAccProvider, nil
@@ -35,8 +29,6 @@ func testAccPreCheck(t *testing.T) {
 	apiEndpoint := os.Getenv("LUMINATE_API_ENDPOINT")
 	if apiEndpoint == "" {
 		t.Fatal("LUMINATE_API_ENDPOINT must be set for acceptance tests")
-	} else {
-		testAccDomain = strings.Replace(apiEndpoint, "api.", "", 1)
 	}
 	if v := os.Getenv("LUMINATE_API_CLIENT_ID"); v == "" {
 		t.Fatal("LUMINATE_API_CLIENT_ID must be set for acceptance tests")
