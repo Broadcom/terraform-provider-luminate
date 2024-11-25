c224f30470fdd98e5f4734367a8494e676199fd7
amirlevinzon <amir.levinzon@broadcom.com>
2024-11-25 16:24:24 +0200

    AC-35211 add TEST_IDP_ID to env

--- provider/resource_group_test.go
+++ provider/resource_group_test.go
@@ -1,27 +1,34 @@
 package provider
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-const testGroupCreate = `
-resource "luminate_resources_group" "new-group" {
+func testGroupCreate(idpID string) string {
+	return fmt.Sprintf(`
+    resource "luminate_resources_group" "new-group" {
 	name = "testGroup"
-	identity_provider_id = "local"
+	identity_provider_id = "%s"
+		}
+	`, idpID)
 }
-`
 
 func TestGroupCreate(t *testing.T) {
 	resourceName := "luminate_resources_group.new-group"
-
+	var idpID string
+	if idpID = os.Getenv("TEST_IDP_ID"); idpID == "" {
+		t.Error("stopping TestGroupCreate no idpID provided")
+	}
 	resource.Test(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: newTestAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testGroupCreate,
+				Config: testGroupCreate(idpID),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "name", "testGroup"),
 				),
