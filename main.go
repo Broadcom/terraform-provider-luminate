f9d08526e4d09c6f85e8c265f34c252eb3cb7e0d
Ben Rubchinsky <ben.rubchinsky@broadcom.com>
2025-03-25 16:57:30 +0200

    AC-39261 | Use terraform-plugin-framework & terraform-plugin-mux

--- main.go
+++ main.go
@@ -1,12 +1,44 @@
 package main
 
 import (
-	"github.com/Broadcom/terraform-provider-luminate/provider"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"context"
+	"flag"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
+	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
+
+	luminateFrameworkProvider "github.com/Broadcom/terraform-provider-luminate/framework_provider"
+	luminateSdkProvider "github.com/Broadcom/terraform-provider-luminate/provider"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: provider.Provider,
-	})
+	withDebug := flag.Bool("debug", false, "runs the provider with debug to allows debuggers such as delve to attach to it")
+	flag.Parse()
+
+	var opts []tf5server.ServeOpt
+	if withDebug != nil && *withDebug {
+		opts = append(opts, tf5server.WithManagedDebug())
+	}
+
+	ctx := context.Background()
+	sdkProvider := luminateSdkProvider.Provider()
+	providers := []func() tfprotov5.ProviderServer{
+		sdkProvider.GRPCProvider,
+		providerserver.NewProtocol5(luminateFrameworkProvider.NewLuminateFrameworkProvider(sdkProvider)),
+	}
+
+	muxServer, err := tf5muxserver.NewMuxServer(ctx, providers...)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	err = tf5server.Serve("broadcom.com/broadcom/luminate", muxServer.ProviderServer, opts...)
+
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
