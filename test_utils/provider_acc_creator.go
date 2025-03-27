0004dccf7c4fc40235a4d64b204eb727d46ccafd
Ben Rubchinsky <ben.rubchinsky@broadcom.com>
2025-03-27 14:07:51 +0200

    AC-39261 | Use terrafrom protocol6 for testing - create test provider with mux server

--- test_utils/provider_acc_creator.go
+++ test_utils/provider_acc_creator.go
@@ -2,19 +2,41 @@ package test_utils
 
 import (
 	"context"
+	luminateFrameworkProvider "github.com/Broadcom/terraform-provider-luminate/framework_provider"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"log"
 
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
 )
 
 func CreateProtoV6ProviderFactories(sdkTestProvider *schema.Provider) map[string]func() (tfprotov6.ProviderServer, error) {
+	ctx := context.Background()
+	upgradedSdkProvider, err := tf5to6server.UpgradeServer(
+		ctx,
+		sdkTestProvider.GRPCProvider,
+	)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	providers := []func() tfprotov6.ProviderServer{
+		func() tfprotov6.ProviderServer {
+			return upgradedSdkProvider
+		},
+		providerserver.NewProtocol6(luminateFrameworkProvider.NewLuminateFrameworkProvider(sdkTestProvider)),
+	}
+
+	muxServer, err := tf6muxserver.NewMuxServer(ctx, providers...)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
 	return map[string]func() (tfprotov6.ProviderServer, error){
 		"luminate": func() (tfprotov6.ProviderServer, error) {
-			return tf5to6server.UpgradeServer(
-				context.Background(),
-				sdkTestProvider.GRPCProvider,
-			)
+			return muxServer.ProviderServer(), nil
 		},
 	}
 }
